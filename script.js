d13fe4a3af89f51d092ad404cd108d6d8a744abc
JulieStuneck <jbstuneck@yahoo.com>
2018-07-08 17:18:44 -0400

    new website

---
+++ script.js
@@ -0,0 +1,33 @@
+const footer = document.querySelector('.content h3');
+footer.addEventListener('mouseover', function (){
+ footer.classList.add('hover');
+})
+footer.addEventListener('mouseout', function (){
+ footer.classList.remove('hover');    
+});
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}

d478af1854dd162cf93819e8ca7b7849fb1724f9
JulieStuneck <jbstuneck@yahoo.com>
2018-07-09 23:00:23 -0400

    bio and project modals done

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
-const footer = document.querySelector('.content h3');
-footer.addEventListener('mouseover', function (){
- footer.classList.add('hover');
-})
-footer.addEventListener('mouseout', function (){
- footer.classList.remove('hover');    
-});
-
-// Get the modal
+/*Bio Button*/
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+/*Project 1*/
+var modal1 = document.getElementById('proj1Modal');
+var btn1 = document.getElementById("proj1Btn");
+var span = document.getElementsByClassName("close1")[0];
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+span.onclick = function() {
+    modal1.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+/*Project 2*/
+var modal2 = document.getElementById('proj2Modal');
+var btn2 = document.getElementById("proj2Btn");
+var span = document.getElementsByClassName("close2")[0];
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+span.onclick = function() {
+    modal2.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
+/*Project 3*/
+var modal3 = document.getElementById('proj3Modal');
+var btn3 = document.getElementById("proj3Btn");
+var span = document.getElementsByClassName("close3")[0];
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+span.onclick = function() {
+    modal3.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
     }
 }

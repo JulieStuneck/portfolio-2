8e898de0a0d02d203f515c8fe2cf1d3ef2ce8a35
JulieStuneck <jbstuneck@yahoo.com>
2018-07-10 00:33:06 -0400

    slide-show for project 3 not working

--- script.js
+++ script.js
@@ -61,3 +61,19 @@ window.onclick = function(event) {
         modal3.style.display = "none";
     }
 }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+   
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000);
+    }
